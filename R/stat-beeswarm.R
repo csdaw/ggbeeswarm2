61f45ee789f1d605f09b1ec3b6d1df362ded2668
Charlotte Dawson <csdaw@outlook.com>
2020-07-18 20:10:25 +0100

    Allow some swarm arguments in stat_beeswarm

--- R/stat-beeswarm.R
+++ R/stat-beeswarm.R
@@ -1,8 +1,10 @@
 StatBeeswarm <- ggproto("StatBeeswarm", Stat,
-                        compute_group = function(data, scales) {
+                        compute_group = function(data, scales, 
+                                                 spacing = 1, side = 0L, 
+                                                 priority = "ascending") {
                           x.offset <- beeswarm::swarmx(
                             x = rep(0, legnth(data$y)), y = data$y,
-                            cex = 1, side = 0L, priority = "ascending"
+                            cex = spacing, side = side, priority = priority
                           )$x
                           
                           data$x <- data$x + x.offset
@@ -14,12 +16,18 @@ StatBeeswarm <- ggproto("StatBeeswarm", Stat,
 
 stat_beeswarm <- function(mapping = NULL, data = NULL, 
                           position = "identity", ..., 
+                          spacing = 1, side = 0L, priority = "ascending",
                           na.rm = FALSE, show.legend = NA, inherit.aes = TRUE) {
+  stopifnot(priority %in% c("ascending", "descending", "density", "random", "none"))
+  
   layer(
     stat = StatBeeswarm, data = data, mapping = mapping, geom = "point", 
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
     params = list(
       na.rm = na.rm, 
+      spacing = spacing,
+      side = side,
+      priority = priority,
       ...
     )
   )
