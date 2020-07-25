b6b62f66166cf2838fcdd6dbbb0f401e28199a18
Charlotte Dawson <csdaw@outlook.com>
2020-07-25 16:08:12 +0100

    Implement offsetX in StatQuasirandom

--- R/stat-quasirandom.R
+++ R/stat-quasirandom.R
@@ -1,11 +1,23 @@
+#' @export
 StatQuasirandom <- ggproto("StatQuasirandom", Stat,
                            compute_group = function(data, scales) {
+                             x.offset <- vipor::offsetX(
+                               data$y,
+                               data$x,
+                               width = 0.4,
+                               varwidth = FALSE,
+                               adjust = 0.5,
+                               method = "quasirandom",
+                               nbins = NULL
+                             )
+                             data$x <- data$x + x.offset
                              data
                            },
                            
                            required_aes = c("x", "y")
 )
 
+#' @export
 stat_quasirandom <- function(mapping = NULL, data = NULL, 
                              position = "identity", ..., 
                              na.rm = FALSE, show.legend = NA, inherit.aes = TRUE) {
