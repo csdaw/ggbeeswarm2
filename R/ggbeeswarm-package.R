fd846cc4f13335e0bc63abeb30bc66ef71085563
Charlotte Dawson <csdaw@outlook.com>
2020-09-07 12:15:26 +0100

    Rewrite quasirandom geom and position

--- R/ggbeeswarm-package.R
+++ R/ggbeeswarm-package.R
@@ -1,20 +1,5 @@
-#' ggbeeswarm extends ggplot2 with violin point/beeswarm plots
-#' 
-#' This package allows plotting of several groups of one dimensional data as a violin point/beeswarm plot by arranging data points to resemble the underlying distribution. The development version of this package is on \url{http://github.com/eclarke/ggbeeswarm}.
-#' @docType package
-#' @name ggbeeswarm
-#' @author Erik Clarke, \email{ecl@@mail.med.upenn.edu}
-#' @seealso \code{\link{position_quasirandom}}
-#' @examples
-#' 
-#'   ggplot2::ggplot(ggplot2::mpg,aes(class, hwy)) + geom_quasirandom()
-#'   # Generate fake data
-#'   distro <- data.frame(
-#'     'variable'=rep(c('runif','rnorm'),each=100),
-#'     'value'=c(runif(100, min=-3, max=3), rnorm(100))
-#'   )
-#'   ggplot2::ggplot(distro,aes(variable, value)) + geom_quasirandom()
-#'   ggplot2::ggplot(distro,aes(variable, value)) + geom_quasirandom(width=.1)
-#' 
-NULL
+#' @keywords internal
+"_PACKAGE"
 
+#' @import ggplot2
+NULL
--- R/position-quasirandom.R
+++ R/position-quasirandom.R
@@ -1,39 +1,94 @@
 #' @export
-position_quasirandom <- function(width = NULL) {
+position_quasirandom <- function(method = "quasirandom",
+                                 width = NULL, varwidth = FALSE,
+                                 bandwidth = 0.5, nbins = NULL, 
+                                 dodge.width = NULL) {
   ggproto(NULL, PositionQuasirandom,
-          width = width
+          method = method,
+          width = width, 
+          varwidth = varwidth,
+          bandwidth = bandwidth, 
+          nbins = nbins, 
+          dodge.width = dodge.width
   )
 }
 
-PositionQuasirandom <- ggproto("PositionQuasirandom", ggplot2::Position, 
-                               width = NULL,
+PositionQuasirandom <- ggproto("PositionQuasirandom", Position, 
+                               required_aes = c("x", "y"),
+
                                setup_params = function(self, data) {
-                                 flipped_aes <- ggplot2::has_flipped_aes(data)
-                                 data <- ggplot2::flip_data(data, flipped_aes)
+                                 flipped_aes <- has_flipped_aes(data)
+                                 data <- flip_data(data, flipped_aes)
+                                 
+                                 # get number of points in each x axis group and 
+                                 # find the largest group
+                                 max.length <- max(data.frame(table(data$x))$Freq)
                                  
                                  list(
+                                   method = self$method,
                                    width = self$width,
+                                   varwidth = self$varwidth,
+                                   bandwidth = self$bandwidth,
+                                   nbins = self$nbins,
+                                   max.length = max.length,
+                                   dodge.width = self$dodge.width,
                                    flipped_aes = flipped_aes
                                  )
                                },
+                               
                                compute_panel = function(data, params, scales) {
                                  data <- flip_data(data, params$flipped_aes)
-                                 data$x <- data$x.orig
                                  
-                                 collided <- ggplot2:::collide(
+                                 # set width if not specified
+                                 if (is.null(params$width)) {
+                                   params$width <- ggplot2::resolution(
+                                     data$x, zero = FALSE) * 0.4
+                                 }
+                                 
+                                 data <- ggplot2:::collide(
                                    data,
-                                   params$width,
+                                   params$dodge.width,
                                    name = "position_quasirandom",
                                    strategy = ggplot2:::pos_dodge,
-                                   n = NULL,
                                    check.width = FALSE
                                  )
-                                 collided$x <- collided$x + collided$x.offset
                                  
-                                 flip_data(collided, params$flipped_aes)
-                               },
-                               required_aes=c('x','y')
-                               
-                               
+                                 # split data.frame into list of data.frames
+                                 if(!is.null(params$dodge.width)) {
+                                   data <- split(data, data$group)
+                                 } else {
+                                   data <- split(data, data$x)
+                                 }
+                                 
+                                 # perform swarming separately for each data.frame
+                                 data <- lapply(
+                                   data,
+                                   pos_quasirandom,
+                                   method = params$method,
+                                   width = params$width,
+                                   vary.width = params$varwidth,
+                                   adjust = params$bandwidth,
+                                   nbins = params$nbins,
+                                   max.length = params$max.length
+                                 )
+                                 
+                                 # recombine list of data.frames into one
+                                 data <- Reduce(rbind, data)
+                                 
+                                 flip_data(data, params$flipped_aes)
+                               }
 )
 
+pos_quasirandom <- function(df, width = 0.4, vary.width = FALSE,
+                            max.length = NULL, ...) {
+  x.offset <- vipor::aveWithArgs(
+    df$y, df$x, 
+    FUN = vipor::offsetSingleGroup,
+    maxLength = if (vary.width) {max.length} else {NULL},
+    ...
+  )
+  
+  x.offset <- x.offset * width
+  df$x <- df$x + x.offset
+  df
+}
