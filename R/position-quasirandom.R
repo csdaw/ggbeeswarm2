06b797069b7d70e003f4294533aa4990ef4f8ff8
Charlotte Dawson <csdaw@outlook.com>
2021-03-13 14:13:58 +0000

    Change default quasirandom width

--- R/position-quasirandom.R
+++ R/position-quasirandom.R
@@ -88,7 +88,7 @@ PositionQuasirandom <- ggproto("PositionQuasirandom", Position,
                                  # set width if not specified
                                  if (is.null(params$width)) {
                                    params$width <- ggplot2::resolution(
-                                     data$x, zero = FALSE) * 0.1
+                                     data$x, zero = FALSE) * 0.05
                                  }
                                  
                                  data <- .beeint$collide(
